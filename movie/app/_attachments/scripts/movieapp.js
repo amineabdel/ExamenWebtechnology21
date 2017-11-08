5de9dbd56e42d6aa8b7f5e28f3b330afedb651b7
amineabdel <amine.oostende@hotmail.com>
2017-11-08 16:21:51 +0100

    movie uploaden 1
    nog niet klaar 1

---
+++ movie/app/_attachments/scripts/movieapp.js
@@ -0,0 +1,61 @@
+'use strict'
+
+
+var c = console; 
+c.log("movie");
+
+angular.module('movieApp', ['ngRoute'])
+
+	.config(function($routeProvider) {
+	    $routeProvider
+	        .when('/home', {
+	            templateUrl: 'assets/views/home.html',
+	            controller: 'homeCtrl'
+	        })
+	           .otherwise({
+                redirectTo: '/home'
+            });
+	     
+	})
+	
+	.controller('homeCtrl', function($scope, actorSrv) {
+		
+		$scope.Actor = "jim Carrey";
+		$scope.movieButton = function(){
+			var actorVal =  $('#ActorText').val();
+			/*$scope.acteur = actorVal;
+			c.log(actorVal);*/
+			
+			actorSrv.getActorList(actorVal).then(function(){
+				c.log("test");
+				$scope.acteur = actorVal;
+				
+			})
+			
+			}
+		
+
+    })
+   
+    .service('actorSrv', function($http, $q) {
+		this.getActorList = function(actorVal) {
+    		var q = $q.defer();
+    		var url = 'http://theimdbapi.org/api/find/person?name=' + encodeURIComponent(actorVal) + '&format=json';
+			
+    		$http.get(url)
+    			.then(function(data){
+    				q.resolve(data);
+    			}, function error(err) {
+    				q.reject(err);
+    			});
+    			
+    			return q.promise;
+    		};
+
+    })
+    
+
+		
+
+
+    
